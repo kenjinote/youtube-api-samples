e92c4b3b3256dfa47ddb6623feb0b654bcf6665e
Andy Diamondstein <adiamondstein@google.com>
2013-01-25 10:11:34 -0800

    "Added sample: python/playlist_updates.py"

---
+++ samples/python/playlist_updates.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+import httplib2
+import os
+import sys
+
+from apiclient.discovery import build
+from oauth2client.file import Storage
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.tools import run
+
+
+# CLIENT_SECRETS_FILE, name of a file containing the OAuth 2.0 information for
+# this application, including client_id and client_secret. You can acquire an
+# ID/secret pair from the API Access tab on the Google APIs Console
+#   http://code.google.com/apis/console#access
+# For more information about using OAuth2 to access Google APIs, please visit:
+#   https://developers.google.com/accounts/docs/OAuth2
+# For more information about the client_secrets.json file format, please visit:
+#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+# Please ensure that you have enabled the YouTube Data API for your project.
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# Helpful message to display if the CLIENT_SECRETS_FILE is missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+
+   %s
+
+with information from the APIs Console
+https://code.google.com/apis/console#access
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
+# An OAuth 2 access scope that allows for full read/write access.
+YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+  message=MISSING_CLIENT_SECRETS_MESSAGE,
+  scope=YOUTUBE_READ_WRITE_SCOPE)
+
+storage = Storage("%s-oauth2.json" % sys.argv[0])
+credentials = storage.get()
+
+if credentials is None or credentials.invalid:
+  credentials = run(flow, storage)
+
+youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+  http=credentials.authorize(httplib2.Http()))
+
+playlists_insert_response = youtube.playlists().insert(
+  part="snippet,status",
+  body=dict(
+    snippet=dict(
+      title="Test Playlist",
+      description="A private playlist created with the YouTube API v3"
+    ),
+    status=dict(
+      privacyStatus="private"
+    )
+  )
+).execute()
+
+print "New playlist id: %s" % playlists_insert_response["id"]

ed9ba723e1e56db222a1a03047bb8af93191be4a
Andy Diamondstein <adiamondstein@google.com>
2013-02-27 12:13:35 -0800

    "Added sample: php/list_broadcasts.php"

---
+++ samples/php/list_broadcasts.php
@@ -0,0 +1,87 @@
+<?php
+// Call set_include_path() as needed to point to your client library.
+require_once 'Google_Client.php';
+require_once 'contrib/Google_YouTubeService.php';
+session_start();
+
+/* You can acquire an OAuth 2 ID/secret pair from the API Access tab on the Google APIs Console
+ <http://code.google.com/apis/console#access>
+For more information about using OAuth2 to access Google APIs, please visit:
+<https://developers.google.com/accounts/docs/OAuth2>
+Please ensure that you have enabled the YouTube Data API for your project. */
+$OAUTH2_CLIENT_ID = 'REPLACE_ME';
+$OAUTH2_CLIENT_SECRET = 'REPLACE_ME';
+
+$client = new Google_Client();
+$client->setClientId($OAUTH2_CLIENT_ID);
+$client->setClientSecret($OAUTH2_CLIENT_SECRET);
+$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+    FILTER_SANITIZE_URL);
+$client->setRedirectUri($redirect);
+
+// YouTube object used to make all API requests.
+$youtube = new Google_YoutubeService($client);
+
+if (isset($_GET['code'])) {
+  if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+    die('The session state did not match.');
+  }
+
+  $client->authenticate();
+  $_SESSION['token'] = $client->getAccessToken();
+  header('Location: ' . $redirect);
+}
+
+if (isset($_SESSION['token'])) {
+  $client->setAccessToken($_SESSION['token']);
+}
+
+// Check if access token successfully acquired
+if ($client->getAccessToken()) {
+  try {
+    // Execute request to list broadcasts.
+    $broadcastsResponse = $youtube->liveBroadcasts->listLiveBroadcasts(
+        'id,snippet',
+        array(
+            'mine' => 'true',
+        ));
+
+    $htmlBody .= "<h3>Live Broadcasts</h3><ul>";
+    foreach ($broadcastsResponse['items'] as $broadcastItem) {
+      $htmlBody .= sprintf('<li>%s (%s)</li>', $broadcastItem['snippet']['title'],
+          $broadcastItem['id']);
+    }
+    $htmlBody .= '</ul>';
+
+  } catch (Google_ServiceException $e) {
+    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+        htmlspecialchars($e->getMessage()));
+  } catch (Google_Exception $e) {
+    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+        htmlspecialchars($e->getMessage()));
+  }
+
+  $_SESSION['token'] = $client->getAccessToken();
+} else {
+  // If the user hasn't authorized the app, initiate the OAuth flow
+  $state = mt_rand();
+  $client->setState($state);
+  $_SESSION['state'] = $state;
+
+  $authUrl = $client->createAuthUrl();
+  $htmlBody = <<<END
+  <h3>Authorization Required</h3>
+  <p>You need to <a href="$authUrl">authorize access</a> before proceeding.<p>
+END;
+}
+?>
+
+<!doctype html>
+<html>
+<head>
+<title>My Live Broadcasts</title>
+</head>
+<body>
+  <?=$htmlBody?>
+</body>
+</html>
